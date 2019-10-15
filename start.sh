85c43ee6dac7674939548df3b726de4cd7568ee6
ygqygq2 <29ygq@sina.com>
2019-10-15 16:12:01 +0800

    fix: 修复oss.conf和oauth.conf不自动生成

--- start.sh
+++ start.sh
@@ -7,6 +7,14 @@ if [ ! -f "/BookStack/conf/app.conf" ] ; then
     cp /BookStack/conf/app.conf.example /BookStack/conf/app.conf
 fi
 
+if [ ! -f "/BookStack/conf/oss.conf" ] ; then
+    cp /BookStack/conf/oss.conf.example /BookStack/conf/oss.conf
+fi
+
+if [ ! -f "/BookStack/conf/oauth.conf" ] ; then
+    cp /BookStack/conf/oauth.conf.example /BookStack/conf/oauth.conf
+fi
+
 export ZONEINFO=/mindoc/lib/time/zoneinfo.zip
 /BookStack/BookStack install
 /BookStack/BookStack
